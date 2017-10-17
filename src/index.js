d74b341eb52f21b4a5a22a12f12853a454cd7592
Henry <18618415652@163.com>
2017-10-17 11:55:58 +0800

    哈哈

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+class Clock extends React.Component {
+    constructor(){
+        super();
+        this.state={date:new Date(),
+        isToggleOn:true,
+         };
+         this.handleClick = this.handleClick.bind(this);
+    }
+   componentDidMount() {
+       this.timerId=setInterval(
+           ()=>this.tick(), 1000
+        )
+
+   }
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+    tick(){
+        this.setState({
+            date:new Date()
+        })
+    }
+    handleClick(){
+        this.setState(prevet=>({
+            isToggleOn: !prevet.isToggleOn
+        }))
+    }
+   
+    render() {
+        return (
+            <div>
+                <h1 className="">Hello World!!</h1>
+                <h2> It is {this.state.date.toLocaleTimeString()}</h2>
+               <button onClick={this.handleClick}>
+               {this.state.isToggleOn?'hello herny':'hello meixiang'}
+               </button>
+                你是谁
+            </div>
+        )
+    }
+}
+
+
+
+    ReactDOM.render(
+        <div>
+        <Clock />
+        <Clock />
+        <Clock />
+        </div>,
+        document.getElementById('root')
+    );
+
+
+
+
+
+
---
+++ src/practise.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+  
+class Bigone extends Component{
+    render(){
+        return(
+          <h1>
+              Hello world
+          </h1>
+        )
+      
+    }
+}
+export default Bigone 
